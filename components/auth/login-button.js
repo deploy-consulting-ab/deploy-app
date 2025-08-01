b784c431f7121b107bfaef09665bcf2b64646e47
agnieto <agnieto@deployconsulting.se>
2025-08-01 13:38:37 +0200

    Initial setup

---
+++ components/auth/login-button.js
@@ -0,0 +1,20 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+export const LoginButton = ({ children, mode = 'redirect', asChild }) => {
+    const router = useRouter();
+    const onClick = () => {
+        router.push('/auth/login');
+    };
+
+    if (mode === 'modal') {
+        return <p>Implement modal mode</p>;
+    }
+
+    return (
+        <span className="cursor-pointer" onClick={onClick}>
+            {children}
+        </span>
+    );
+};
