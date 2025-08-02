d8c9c0bef504e6f81a058e045f9fef572f82551a
agnieto <agnieto@deployconsulting.se>
2025-08-03 00:27:39 +0200

    Implement logout

---
+++ actions/logout.js
@@ -0,0 +1,9 @@
+'use server';
+import { signOut } from "@/auth";
+import { LOGIN_ROUTE } from "@/routes";
+
+export const logout = async () => {
+    await signOut({
+        redirectTo : LOGIN_ROUTE // This was not happening automatically, might be a bug
+    });
+};
--- app/(protected)/settings/page.js
+++ app/(protected)/settings/page.js
@@ -1,9 +1,18 @@
 import { auth } from '@/auth';
+import { logout } from '@/actions/logout';
+
 const SettingsPage = async () => {
 
     const session = await auth();
 
-    return <div>{JSON.stringify(session)}</div>;
+    return <div>
+        {JSON.stringify(session)}
+        <form action={logout}>
+            <button type="submit">
+                Sign out
+            </button>
+        </form>
+    </div>;
 };
 
 export default SettingsPage;
