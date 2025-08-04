d87a48f5c2e1d5ac6c38d7b0190cd29248723dec
agnieto <agnieto@deployconsulting.se>
2025-08-04 19:27:33 +0200

    Add callback

--- auth.js
+++ auth.js
@@ -2,9 +2,53 @@ import NextAuth from 'next-auth';
 import authConfig from '@/auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/lib/db';
+import { getUserById } from '@/data/user';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     ...authConfig,
     session: { strategy: 'jwt' },
     adapter: PrismaAdapter(db),
+    callbacks: {
+        async signIn({ user }) {
+            const existingUser = await getUserById(user.id);
+
+            if (!existingUser) {
+                return false;
+            }
+
+            return true;
+        },
+        async session({ session, token }) {
+            console.log('to', token);
+
+            if (!session.user) {
+                return session;
+            }
+
+            if (token.sub) {
+                session.user.sessionId = token.sub;
+            }
+
+            if (token.salesforceId) {
+                session.user.salesforceId = token.salesforceId;
+            }
+
+            return session;
+        },
+        async jwt({ token }) {
+            // Check if I am logged out
+            if (!token.sub) {
+                return token;
+            }
+
+            // Token sub equals to the user id in the database
+            const loggedUser = await getUserById(token.sub);
+            if (!loggedUser) {
+                return token;
+            }
+
+            token.salesforceId = loggedUser['salesforce_id'];
+            return token;
+        },
+    },
 });
