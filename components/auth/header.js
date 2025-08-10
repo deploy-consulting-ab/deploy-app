8cfc45a4c8a480d86158c15b15b40c9206811c74
álvaro <agnieto00@gmail.com>
2025-08-11 00:34:53 +0200

    Add logo css

--- components/auth/header.js
+++ components/auth/header.js
@@ -10,7 +10,7 @@ const font = Poppins({
 export const HeaderComponent = ({ label }) => {
     return (
         <div className="w-full flex flex-col pt-2 items-center justify-center">
-            <DeployLogo className="h-10 w-auto z-[99] relative" />
+            <DeployLogo className="dark-logo h-10 w-auto z-[99] relative" />
         </div>
     );
 };
