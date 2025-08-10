c9d79d566f6de803673c3329e4090dbaef70939a
álvaro <agnieto00@gmail.com>
2025-08-10 13:05:59 +0200

    Remove icon

--- components/auth/header.js
+++ components/auth/header.js
@@ -9,7 +9,7 @@ const font = Poppins({
 export const HeaderComponent = ({ label }) => {
     return (
         <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-            <h1 className={cn('text-3xl font-semibold', font.className)}>🔓 Deploy</h1>
+            <h1 className={cn('text-3xl font-semibold', font.className)}>Deploy</h1>
             <p className="text-muted-foreground text-sm">{label}</p>
         </div>
     );
