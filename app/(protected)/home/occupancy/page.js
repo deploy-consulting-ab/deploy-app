d75e58961cb709d805d8d9231a3d9f9c756153c7
álvaro <agnieto00@gmail.com>
2025-08-12 14:11:31 +0200

    Add basic chart

--- app/(protected)/home/occupancy/page.js
+++ app/(protected)/home/occupancy/page.js
@@ -1,12 +1,18 @@
 import { OccupancyCard } from '@/components/application/occupancy-card';
 import { employeeData } from '@/lib/mock-data';
+import { OccupancyChartComponent } from '@/components/application/occupancy/occupancy-chart';
 
 export default function OccupancyPage() {
     return (
         <div className="flex flex-col min-h-screen">
             <div className="flex-1 space-y-8 pt-4">
-                <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
-                    <OccupancyCard occupancy={employeeData.occupancy} />
+                <div className="grid gap-4 grid-cols-1">
+                    <div className="w-full">
+                        <OccupancyChartComponent />
+                    </div>
+                    <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
+                        <OccupancyCard occupancy={employeeData.occupancy} />
+                    </div>
                 </div>
             </div>
         </div>
