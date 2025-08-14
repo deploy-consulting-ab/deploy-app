b07b9c55604294b8ff129a5f00679f10ca9b9d9c
álvaro <agnieto00@gmail.com>
2025-08-14 23:29:20 +0200

    Remove log

--- app/(protected)/home/opportunities/page.js
+++ app/(protected)/home/opportunities/page.js
@@ -6,8 +6,6 @@ import { getOpportunities } from '@/actions/salesforce/salesforce-actions';
 const OpportunitiesPage = async () => {
     const opportunities = await getOpportunities();
 
-    console.log('opportunities...', opportunities);
-
     return (
         <div className="py-4">
             <OpportunitiesListComponent opportunities={opportunities}/>
