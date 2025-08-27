4ca459e4c461f70cca42ff69761c6fc109b52097
álvaro <agnieto@deployconsulting.se>
2025-08-27 21:58:36 +0200

    Change auth config files

--- auth.config.js
+++ auth.config.js
@@ -12,19 +12,7 @@ const authObject = {
                 if (validatedFields.success) {
                     const { email, password } = validatedFields.data;
 
-                    // FAKE USER FOR TESTING AND PREVENT NEON OVERLOADED
-                    return {
-                        salesforce_id: '001234567890',
-                        employee_number: 'D00082', // This is not changed here, but in auth
-                        email: 'asdasd@test.com',
-                        name: 'KOLASP',
-                        role: 'admin',
-                        department: 'IT',
-                        location: 'New York',
-                    }
-
                     const user = await getUserByEmail(email);
-                    
 
                     // No user, or user does not have a password (Google auth)
                     if (!user || !user.password) {
@@ -48,7 +36,7 @@ const authObject = {
     session: {
         strategy: 'jwt',
         maxAge: 6 * 60 * 60, // 12 hours in seconds
-        updateAge: 60 * 60,   // Optional: refresh session every hour
+        updateAge: 60 * 60, // Optional: refresh session every hour
     },
 };
 
--- auth.js
+++ auth.js
@@ -9,10 +9,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(db),
     callbacks: {
         async signIn({ user }) {
-
-            // FAKE USER FOR TESTING AND PREVENT NEON OVERLOADED
-            return true;
-
             const existingUser = await getUserById(user.id);
 
             if (!existingUser) {
@@ -51,27 +47,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             }
 
             // Token sub equals to the user id in the database
-            // const loggedUser = await getUserById(token.sub);
-            
-            // FAKE USER FOR TESTING AND PREVENT NEON OVERLOADED
-            const loggedUser = {
-                salesforce_id: '001234567890',
-                employee_number: 'D003',
-                email: 'asas@test.com',
-                name: 'UUJEKS',
-                role: 'admin',
-                department: 'IT',
-                location: 'New York',
-                phone: '1234567890',
-                address: '123 Main St, Anytown, USA',
-            }
-
-            console.info('Remove user', loggedUser);
-
+            const loggedUser = await getUserById(token.sub);
             if (!loggedUser) {
                 return token;
             }
-            
 
             token.salesforceId = loggedUser['salesforce_id'];
             token.employeeNumber = loggedUser['employee_number'];
