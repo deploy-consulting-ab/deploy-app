41e84f1b5db30711702e01db587f65b4591fe5bc
álvaro <agnieto@deployconsulting.se>
2025-08-28 19:30:23 +0200

    Remove log

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -2,8 +2,6 @@ import { LoginFormComponent } from '@/components/auth/login-form';
 import { LoginFormGoogleComponent } from '@/components/auth/login-form-google';
 
 const LoginPage = () => {
-    console.log(process.env.LOGIN_METHOD);
-
     const loginMethod = process.env.LOGIN_METHOD;
 
     return <>{loginMethod === 'google' ? <LoginFormGoogleComponent /> : <LoginFormComponent />}</>;
