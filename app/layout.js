64e22354fc10528e05f452c464f0efc72187f500
álvaro <agnieto@deployconsulting.se>
2025-09-02 16:42:45 +0200

    Fix indentation

--- app/layout.js
+++ app/layout.js
@@ -1,52 +1,48 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Analytics } from "@vercel/analytics/next"
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
 });
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Tilde",
-  description: "Tilde App",
-  icons: {
-    icon: [
-      {
-        url: '/images/favicon.png',
-        sizes: '64x64',
-        type: 'image/png',
-      },
-    ],
-  },
+    title: 'Tilde',
+    description: 'Tilde App',
+    icons: {
+        icon: [
+            {
+                url: '/images/favicon.png',
+                sizes: '64x64',
+                type: 'image/png',
+            },
+        ],
+    },
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" suppressHydrationWarning className="h-full">
-      <body
-        className={`${inter.className} antialiased h-full`}
-      >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-        >
-          <div className="h-full bg-background">
-            {children}
-          </div>
-        </ThemeProvider>
-        <Analytics />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning className="h-full">
+            <body className={`${inter.className} antialiased h-full`}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <div className="h-full bg-background">{children}</div>
+                </ThemeProvider>
+                <Analytics />
+            </body>
+        </html>
+    );
 }
