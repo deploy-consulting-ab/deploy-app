1f024c0a54599c477fb51a914e58b093ec3fbeac
álvaro <agnieto@deployconsulting.se>
2025-09-02 17:20:12 +0200

    Add spinner

--- components/auth/login-form-google.js
+++ components/auth/login-form-google.js
@@ -5,13 +5,19 @@ import { Button } from '@/components/ui/button';
 import { CardWrapperComponent } from '@/components/auth/card-wrapper';
 import { loginGoogle } from '@/actions/login-google';
 import { useSearchParams } from 'next/navigation';
+import { useTransition } from 'react';
+import { Spinner } from '@/components/ui/spinner';
 
 export const LoginFormGoogleComponent = () => {
     const searchParams = useSearchParams();
     const callbackUrl = searchParams.get('callbackUrl');
 
+    const [isPending, startTransition] = useTransition();
+
     const handleGoogleLogin = () => {
-        loginGoogle(callbackUrl);
+        startTransition(async () => {
+            await loginGoogle(callbackUrl);
+        });
     };
 
     return (
@@ -22,12 +28,23 @@ export const LoginFormGoogleComponent = () => {
                 </p>
                 <Button
                     size="lg"
-                    className="w-full shadow-box dark:bg-white dark:hover:bg-white/60 flex items-center justify-center gap-x-2"
+                    className="w-full shadow-box dark:bg-white dark:hover:bg-white/60 flex items-center justify-center gap-x-2 hover:cursor-pointer"
                     variant="outline"
                     onClick={handleGoogleLogin}
+                    disabled={isPending}
                 >
-                    <FcGoogle className="h-5 w-5" />
-                    <span className="dark:text-black">Continue with Google</span>
+                    {isPending ? (
+                        <>
+                            <Spinner size="sm" variant="primary" />
+                            <FcGoogle className="h-5 w-5" />
+                            <span className="dark:text-black">Continue with Google</span>
+                        </>
+                    ) : (
+                        <>
+                            <FcGoogle className="h-5 w-5" />
+                            <span className="dark:text-black">Continue with Google</span>
+                        </>
+                    )}
                 </Button>
             </div>
         </CardWrapperComponent>
