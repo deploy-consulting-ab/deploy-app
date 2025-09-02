acfea1930ff2eacdf26534324c80da4ab9bffcc6
álvaro <agnieto@deployconsulting.se>
2025-09-02 12:48:01 +0200

    Get image

--- components/application/sidebar/app-sidebar-user.js
+++ components/application/sidebar/app-sidebar-user.js
@@ -34,7 +34,7 @@ export function AppSidebarUserComponent({ user }) {
                             className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                         >
                             <Avatar className="h-8 w-8 rounded-lg">
-                                <AvatarImage src={user.avatar} alt={user.name} />
+                                <AvatarImage src={user.image} alt={user.name} />
                                 <AvatarFallback className="rounded-lg">{initials}</AvatarFallback>
                             </Avatar>
                             <div className="grid flex-1 text-left text-sm leading-tight">
@@ -53,7 +53,7 @@ export function AppSidebarUserComponent({ user }) {
                         <DropdownMenuLabel className="p-0 font-normal">
                             <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                 <Avatar className="h-8 w-8 rounded-lg">
-                                    <AvatarImage src={user.avatar} alt={user.name} />
+                                    <AvatarImage src={user.image} alt={user.name} />
                                     <AvatarFallback className="rounded-lg">
                                         {initials}
                                     </AvatarFallback>
