62bf88f1c408fc410053760ad29dddbd3a39e72d
álvaro <agnieto@deployconsulting.se>
2025-09-05 11:16:35 +0200

    Fix spacing

--- actions/flex/flex-actions.js
+++ actions/flex/flex-actions.js
@@ -1,61 +1,53 @@
-"use server";
+'use server';
 
-import { NoResultsError, NetworkError, ApiError } from "../callouts/errors.js";
-import {
-  calculateHolidays,
-  calculateNextResetDate,
-  generateDateRange,
-} from "@/lib/utils.js";
-import { getFlexApiService } from "./flex-service.js";
+import { NoResultsError, NetworkError, ApiError } from '../callouts/errors.js';
+import { calculateHolidays, calculateNextResetDate, generateDateRange } from '@/lib/utils.js';
+import { getFlexApiService } from './flex-service.js';
 
 export async function getAbsenceApplications(employeeNumber, options = { cache: 'force-cache' }) {
-  try {
-    const flexApiClient = await getFlexApiService();
-    flexApiClient.config.cache = options.cache;
-    
-    const response = await flexApiClient.getAbsenceApplications(employeeNumber);
+    try {
+        const flexApiClient = await getFlexApiService();
+        flexApiClient.config.cache = options.cache;
 
-    if (!response?.Result) {
-      throw new NoResultsError("No holidays found");
+        const response = await flexApiClient.getAbsenceApplications(employeeNumber);
+
+        if (!response?.Result) {
+            throw new NoResultsError('No holidays found');
+        }
+
+        const holidays = calculateHolidays(response.Result);
+
+        holidays.totalHolidays = 30; // Potentially get from flex
+        holidays.availableHolidays = holidays.totalHolidays - holidays.currentFiscalUsedHolidays;
+        holidays.recentHolidayPeriods = holidays.holidayPeriods.slice(0, 3);
+        holidays.nextResetDate = calculateNextResetDate(new Date());
+        holidays.allHolidaysRange = [];
+
+        for (const holiday of holidays.holidayPeriods) {
+            holidays.allHolidaysRange.push(...generateDateRange(holiday.fromDate, holiday.toDate));
+        }
+
+        return holidays;
+    } catch (error) {
+        console.error('Error in getAbsenceApplications:', {
+            name: error.name,
+            message: error.message,
+            status: error.status,
+            code: error.code,
+        });
+
+        if (error instanceof NoResultsError) {
+            throw error;
+        }
+
+        if (error instanceof NetworkError || error instanceof ApiError) {
+            throw error;
+        }
+
+        throw new ApiError(
+            error.message || 'Failed to fetch absence applications',
+            error.status,
+            error.code
+        );
     }
-
-    const holidays = calculateHolidays(response.Result);
-
-    holidays.totalHolidays = 30; // Potentially get from flex
-    holidays.availableHolidays =
-      holidays.totalHolidays - holidays.currentFiscalUsedHolidays;
-    holidays.recentHolidayPeriods = holidays.holidayPeriods.slice(0, 3);
-    holidays.nextResetDate = calculateNextResetDate(new Date());
-    holidays.allHolidaysRange = [];
-
-    for (const holiday of holidays.holidayPeriods) {
-      holidays.allHolidaysRange.push(...generateDateRange(
-        holiday.fromDate,
-        holiday.toDate
-      ));
-    }
-
-    return holidays;
-  } catch (error) {
-    console.error("Error in getAbsenceApplications:", {
-      name: error.name,
-      message: error.message,
-      status: error.status,
-      code: error.code,
-    });
-
-    if (error instanceof NoResultsError) {
-      throw error;
-    }
-
-    if (error instanceof NetworkError || error instanceof ApiError) {
-      throw error;
-    }
-
-    throw new ApiError(
-      error.message || "Failed to fetch absence applications",
-      error.status,
-      error.code
-    );
-  }
 }
--- actions/flex/flex-service.js
+++ actions/flex/flex-service.js
@@ -1,54 +1,54 @@
-"use server";
+'use server';
 
-import { CalloutService } from "../callouts/callout-service.js";
-import { FLEX_API_CONFIG } from "./flex-config.js";
-import { ENV } from "./env.js";
+import { CalloutService } from '../callouts/callout-service.js';
+import { FLEX_API_CONFIG } from './flex-config.js';
+import { ENV } from './env.js';
 
 class FlexApiService extends CalloutService {
-  constructor() {
-    super(FLEX_API_CONFIG);
-  }
+    constructor() {
+        super(FLEX_API_CONFIG);
+    }
 
-  // Helper method to add common Flex parameters
-  addFlexParams(params = {}) {
-    return {
-      ...params,
-      instance: ENV.FLEX_API_INSTANCE,
-      companynumber: ENV.FLEX_API_COMPANY_NUMBER,
-    };
-  }
+    // Helper method to add common Flex parameters
+    addFlexParams(params = {}) {
+        return {
+            ...params,
+            instance: ENV.FLEX_API_INSTANCE,
+            companynumber: ENV.FLEX_API_COMPANY_NUMBER,
+        };
+    }
 
-  // Override get method to automatically add Flex-specific parameters
-  async get(endpoint, options = {}) {
-    const flexOptions = {
-      ...options,
-      params: this.addFlexParams(options.params),
-      headers: {
-        ...options.headers,
-        "Service-Version": "1.0",
-      },
-    };
-    return super.get(endpoint, flexOptions);
-  }
+    // Override get method to automatically add Flex-specific parameters
+    async get(endpoint, options = {}) {
+        const flexOptions = {
+            ...options,
+            params: this.addFlexParams(options.params),
+            headers: {
+                ...options.headers,
+                'Service-Version': '1.0',
+            },
+        };
+        return super.get(endpoint, flexOptions);
+    }
 
-  // Specialized methods for Flex API endpoints
-  async getAbsenceApplications(employeeNumber) {
-    return this.get(FLEX_API_CONFIG.endpoints.absenceApplications, {
-      params: {
-        employmentnumber: employeeNumber,
-      },
-    });
-  }
+    // Specialized methods for Flex API endpoints
+    async getAbsenceApplications(employeeNumber) {
+        return this.get(FLEX_API_CONFIG.endpoints.absenceApplications, {
+            params: {
+                employmentnumber: employeeNumber,
+            },
+        });
+    }
 }
 
 // Create and export a singleton instance
 let instance = null;
 
 export async function getFlexApiService() {
-  if (!instance) {
-    instance = new FlexApiService();
-  }
-  return instance;
+    if (!instance) {
+        instance = new FlexApiService();
+    }
+    return instance;
 }
 
 // Export the class for testing or custom instances if needed
