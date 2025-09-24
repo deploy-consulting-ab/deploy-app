c2ff0380d62fe793d84126c24af96ed943c5fb09
álvaro <agnieto@deployconsulting.se>
2025-09-24 21:26:36 +0200

    Try fix phone video issue

--- app/auth/layout.js
+++ app/auth/layout.js
@@ -1,7 +1,7 @@
 const AuthLayout = ({ children }) => {
     return (
         <div className="h-full flex items-center justify-center relative">
-            <video autoPlay loop muted className="absolute inset-0 w-full h-full object-cover z-0">
+            <video autoPlay loop muted playsInline className="absolute inset-0 w-full h-full object-cover z-0">
                 <source src="/deploy-background.mp4" type="video/mp4" />
             </video>
             <div className="relative z-10">{children}</div>
