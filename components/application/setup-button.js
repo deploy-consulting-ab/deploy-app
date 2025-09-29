f5eaa7a2f2b0aecdbc62170d7edf16005efc3a40
álvaro <agnieto@deployconsulting.se>
2025-09-29 11:16:49 +0200

    Check isMobile

--- components/application/setup-button.js
+++ components/application/setup-button.js
@@ -4,10 +4,17 @@ import Link from 'next/link';
 import { SETUP_ROUTE } from '@/menus/routes';
 import { Button } from '@/components/ui/button';
 import { Settings } from 'lucide-react';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 export function SetupButtonComponent() {
+    const isMobile = useIsMobile();
+
+    if (isMobile) {
+        return null;
+    }
+
     return (
-        <Link href={SETUP_ROUTE} className="hover:cursor-pointer">
+            <Link href={SETUP_ROUTE} className="hover:cursor-pointer">
             <Button variant="outline" size="icon">
                 <Settings className="h-[1.2rem] w-[1.2rem]" />
             </Button>
