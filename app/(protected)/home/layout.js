614bd0540708e8dd1d0eb34518a288094eb09e36
álvaro <agnieto@deployconsulting.se>
2026-01-20 00:31:29 +0100

    Fix rounded border

--- app/(protected)/home/layout.js
+++ app/(protected)/home/layout.js
@@ -16,7 +16,7 @@ export default async function HomeLayout({ children }) {
             <AppSidebarComponent user={user} location="home" />
             <SidebarInset className="bg-sidebar">
                 <AppHeaderComponent location="home" />
-                <main className="flex-1 bg-background rounded-tl-3xl p-4 md:p-6">{children}</main>
+                <main className="flex-1 bg-background md:rounded-tl-3xl p-4 md:p-6 mt-4">{children}</main>
             </SidebarInset>
         </SidebarProvider>
     );
